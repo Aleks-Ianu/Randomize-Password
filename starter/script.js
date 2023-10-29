2b967f513c9bdb8b72164c5e6c885bce5b35f090
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2023-10-29 13:17:12 +0000

    create a function to check if user has selected at least one set of characters and created an empty array to store each set that the user wants to be used for the password into an empty array

--- starter/script.js
+++ starter/script.js
@@ -88,43 +88,61 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-
-
-// create a variable to store the value of the prompt of how many characters the password should generate
-var passwordLength = prompt("Choose the length of the password you want to generate between 8 and 128 characters.");
-// change the value of the variable from string to a numeric value
-passwordLength = +passwordLength;
-// console.log(passwordLength);
-// check if the length is between 8 to 128 characters
-if (passwordLength >= 8 && passwordLength <= 128) {
-    // create confirms to ask the user what type of caracters should the password include 
-    var lowercaseCar = confirm("Do you want to include lowercase characters in your password?");
-    // console.log(lowercaseCar);
-    var upperCaseCar = confirm("Do you want to include upperCase characters in your password?");
-    // console.log(upperCaseCar);
-    var numericCar = confirm("Do you want to include numbers in your password?");
-    // console.log(numericCar);
-    var specialCar = confirm("Do you want to include special characters in your password?");
-    // console.log(specialCar);
-} else {
-  alert("The length of the password has to be bewteen 8 and 128 characters, please try again with a valid number");
-}
-
-
-// Function to prompt user for password options
+var passwordOptions = {};
+//Function to prompt user for password options
 function getPasswordOptions() {
+  //create a variable to store the value of the prompt of how many characters the password should generate
+  var passwordLength = prompt("Choose the length of the password you want to generate between 8 and 128 characters.");
+  // change the value of the variable from string to a numeric value
+  passwordLength = +passwordLength;
+  // check if the length is between 8 to 128 characters
+  if (passwordLength >= 8 && passwordLength <= 128) {
+      // create confirms to ask the user what type of caracters should the password include 
+      var lowerCaseChar = confirm("Do you want to include lowercase characters in your password?");
+      var upperCaseChar = confirm("Do you want to include upperCase characters in your password?");
+      var numericChar = confirm("Do you want to include numbers in your password?");
+      var specialChar = confirm("Do you want to include special characters in your password?");
 
+       passwordOptions = {
+        lowerCase : lowerCaseChar,
+        upperCase : upperCaseChar,
+        numeric : numericChar,
+        specialCase : specialChar
+      };
+  } else {
+    alert("The length of the password has to be bewteen 8 and 128 characters, please try again with a valid number");
+  };
 }
 
+
+  
 // Function for getting a random element from an array
 function getRandom(arr) {
-
 }
 
 // Function to generate password with user input
+// Check if the user has selected at least one set of Characters
 function generatePassword() {
-
+  if (passwordOptions.lowerCase !== true && passwordOptions.upperCase !== true && passwordOptions.numeric !== true && passwordOptions.specialCase !== true ) {
+    alert("You need to specify at least one set of characters, try again.");
+  }
+  // create an empty array to concatenate every true character set
+  var charSet = [];
+  // Concatenate the true conditionals in to the empty array
+  if (passwordOptions.lowerCase) {
+    charSet = charSet.concat(lowerCasedCharacters);
+  }
+  if (passwordOptions.upperCase ) {
+    charSet = charSet.concat(upperCasedCharacters);
+  }
+  if (passwordOptions.numeric) {
+    charSet = charSet.concat(numericCharacters);
+  }
+  if (passwordOptions.specialCase) {
+    charSet = charSet.concat(specialCharacters);
+  }
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
@@ -139,3 +157,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
